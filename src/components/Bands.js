d7c9d6046e624b4bb9ce0a1998cd9d3607d2f62b
Christopher <christopher.black.2209@googlemail.com>
2022-01-30 22:04:18 +0000

    commit tree c84fb7421014433f4ca323d666abbb029d9d6f7a

---
+++ src/components/Bands.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+import Band from "./Band";
+
+class Bands extends Component {
+    renderBands = () => {
+        return this.props.bands.map(band => (
+            <Band key={band.id} band={band} delete={this.props.deleteBand} />
+        ));
+    }
+
+    render() {
+        return (
+            <ol>
+                {this.renderBands()}
+            </ol>
+        )
+    }
+
+}
+
+export default Bands;
